8d19adfa7be8860a77fe5a5b89169cce0be83d79
Jakub Słowiak <slowiakjakub@outlook.com>
2022-12-15 15:41:22 +0100

    Add project files.

---
+++ LIbrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LIbrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
